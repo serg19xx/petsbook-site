7010fafed3abd5554a5463dc99fd29df1b800078
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-04-30 14:18:20 -0400

    Проверяем локал сторедж

--- src/stores/AuthStore.js
+++ src/stores/AuthStore.js
@@ -57,8 +57,9 @@ export const useAuthStore = defineStore('auth', () => {
         const receivedToken = response.data.data.token
 
         token.value = receivedToken
+        console.log('AV 333: Сщхраняем:', receivedToken)
         localStorage.setItem('token', receivedToken)
-        console.log('AV 3:  Token to save:', receivedToken)
+        console.log('AV 444: Читаем:', localStorage.getItem('token'))
         api.defaults.headers.common['Authorization'] = `Bearer ${receivedToken}`
 
         // Получаем данные пользователя через единую функцию
