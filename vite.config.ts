390397bad0b840cb0632e5ba7af2005bf8d310c2
Serg Kostyuk <serg.kostyuk@gmail.com>
2025-06-14 14:34:57 -0400

    BAckup

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
@@ -40,10 +39,5 @@ export default defineConfig({
     port: 4173,
     host: true,
     allowedHosts: true,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
 })
